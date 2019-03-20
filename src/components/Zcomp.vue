0bd0f09e4edbb99a457d46c2b00bf70738d1d87b
kixv <294199673@qq.com>
2019-03-20 16:40:11 +0800

    'add'

---
+++ src/components/Zcomp.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{count}}</h1>
+    <button @click="increase">+</button>
+    <button @click="decrease">-</button>
+  </div>
+</template>
+<script>
+export default{
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increase () {
+      this.count++
+    },
+    decrease () {
+      this.count--
+    }
+  }
+}
+</script>
+<style lang="less">
+  div{
+    h1{
+      font-size:20px;
+    }
+    button{
+      width:100px;
+    }
+  }
+</style>
