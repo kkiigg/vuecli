e7d44d43133c0da7b6f0e26ce3a44a2d6fd2cde6
kixv <294199673@qq.com>
2019-03-21 18:49:37 +0800

    edit sth ,like comp

---
+++ src/components/text.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    {{text}}
+  </div>
+</template>
+import $ from 'jquery'
+<script>
+export default {
+  data () {
+    return {
+      text: 'i am comp'
+    }
+  },
+  methods: {
+    increase () {
+      this.count++
+    }
+  }
+}
+</script>
+<style lang="less">
+div {
+  transition: all 0.3s;
+  h1 {
+    font-size: 20px;
+  }
+  button {
+    width: 100px;
+  }
+}
+</style>
